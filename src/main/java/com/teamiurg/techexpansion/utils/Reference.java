6b455f42377a76d977337a0d7d7fd266a3fa0b27
Dak Smyth <willjp2006@gmail.com>
2016-05-08 07:58:53 -0500

    Update Reference.java

--- src/main/java/com/teamiurg/techexpansion/utils/Reference.java
+++ src/main/java/com/teamiurg/techexpansion/utils/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "TechExpansion";
 	public static final String MOD_NAME = "TechExpansion";
-	public static final String VERSION = "1.0-Reimagined";
+	public static final String VERSION = "0.1-R1";
 	public static final String CLIENT_PROXY_LOCATION = "com.teamiurg.techexpansion.engine.proxy.ClientProxy";
 	public static final String SERVER_PROXY_LOCATION = "com.teamiurg.techexpansion.engine.proxy.CommonProxy";
 }
