41522541a3652129816e90dd85535f9f05735942
ThePuzzlemaker <willjp2006@gmail.com>
2016-12-31 13:18:36 -0600

    Add files for 1.10 tools

--- src/main/java/com/teamiurg/techexpansion/item/TEItems.java
+++ src/main/java/com/teamiurg/techexpansion/item/TEItems.java
@@ -2,7 +2,7 @@ package com.teamiurg.techexpansion.item;
 
 import com.teamiurg.techexpansion.TechExpansion;
 import com.teamiurg.techexpansion.engine.render.RenderHandler;
-//import com.teamiurg.techexpansion.item.tools.TETools;
+import com.teamiurg.techexpansion.item.tools.TETools;
 
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -19,7 +19,7 @@ public class TEItems {
 		GameRegistry.register(copper_ingot);
 		GameRegistry.register(aluminum_ingot);
 		GameRegistry.register(tin_ingot);
-		//TETools.registerItems();
+		TETools.registerItems();
 	}
 
 	public static void registerRenders() {
@@ -27,6 +27,6 @@ public class TEItems {
 		RenderHandler.registerRender(copper_ingot);
 		RenderHandler.registerRender(aluminum_ingot);
 		RenderHandler.registerRender(tin_ingot);
-		//TETools.registerRenders();
+		TETools.registerRenders();
 	}
 }
