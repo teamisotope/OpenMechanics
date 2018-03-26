8e77de3c7cc80c583d89763826e32be588f76f77
Dak Smyth <ThePuzzlemaker@users.noreply.github.com>
2018-03-25 20:35:24 -0500

    Refactoring for name change

--- src/main/scala/com/teamisotope/techexpansion/util/Ref.scala
+++ src/main/scala/com/teamisotope/techexpansion/util/Ref.scala
@@ -4,7 +4,7 @@ object Ref {
 
   final val MODID = "techexpansion"
   final val VERSION = "v2.0-1.10.2"
-  final val NAME = "TechExpansion"
+  final val NAME = "OpenMechanics"
   final val CLIENT_PROXY = "com.teamisotope.techexpansion.proxy.ClientProxy"
   final val SERVER_PROXY = "com.teamisotope.techexpansion.proxy.ServerProxy"
 
